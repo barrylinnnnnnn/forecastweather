a178bdaa438ecb9c64d497f115e31e6ff9543213
lin Barry <barrylinnnnnnn@163.com>
2020-06-19 11:12:37 +0800

    vue

---
+++ js/main.js
@@ -0,0 +1,26 @@
+ var app = new Vue({
+     el:"#app",
+     data:{
+         city:'',
+         weatherList:[]
+     },
+     methods: {
+         searchWeather:function(){
+            //  console.log('天气查询');
+            //  console.log(this.city);
+            var that = this;
+            axios.get('http://wthrcdn.etouch.cn/weather_mini?city='+this.city)
+            .then(function(response){
+                // console.log(response);
+                console.log(response.data.data.forecast);
+                that.weatherList = response.data.data.forecast;
+            })
+            .catch(function(err){})
+         },
+         changeCity:function(city){
+            this.city = city;
+            console.log(city);
+            this.searchWeather();
+         }
+     },
+ })
